b45dac67c7d12ad59272841b5f2ac8750f2d70fa
ashsin19 <sinha.ashish19@gmail.com>
2025-02-16 23:14:43 +0000

    Auth Context Issue Patches #2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 // src/Login.js
 import React, { useState, useEffect }  from 'react';
-import { useAuth } from "../context/AuthContext";
+import { AuthContext } from "../context/AuthContext";
 import axios from 'axios';
 import '../assets/styles/Login.css';
 import loadRuntimeConfig  from '../components/config';
@@ -10,7 +10,7 @@ const Login = ({ onLogin }) => {
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
   const [BASE_URL, setBackendUrl] = useState("");
-  const { setIsLoggedIn, setToken, setUsername } = useAuth();
+  const { handleLogin } = useAuth();
 
   useEffect(() => {
     const fetchConfig = async () => {
@@ -36,9 +36,7 @@ const Login = ({ onLogin }) => {
        const { access_token } = response.data;
        localStorage.setItem('token', access_token); // Save the token in localStorage
        localStorage.setItem("username", localUsername)
-       setIsLoggedIn(true);
-       setToken(access_token);
-       setUsername(localUsername);
+       handleLogin(localUsername, access_token);
       onLogin();
       setMessage('Login successful!');
     } catch (error) {
