6b8eeafef2a94e818b51bae255f0f0b6757790f3
ashsin19 <sinha.ashish19@gmail.com>
2025-02-16 18:58:32 +0000

    Update config.js

--- src/components/config.js
+++ src/components/config.js
@@ -2,26 +2,19 @@ const loadRuntimeConfig = async () => {
   try {
     const response = await fetch("https://storage.googleapis.com/rag-app-deployments_cloudbuild/runtime-config.json");
     if (!response.ok) throw new Error("Failed to load runtime config");
-
     const config = await response.json();
-    console.log("Loaded runtime config:", config);
-
-    // Determine BASE_URL based on environment
-    let backendUrl = "https://python-rag-app-369543119888.us-central1.run.app"; // Default to production
-    if (config.environment === "DEV") {
-      backendUrl = "http://localhost:8080"; // Use local backend in development
-    }
-
-    return {
-      ...config,
-      REACT_APP_BACKEND_URL: backendUrl,
-    };
+    if (config.ENVIRONMENT == 'PROD') 
+      {
+        console.log("PROD ENV");
+        return { REACT_APP_BACKEND_URL: config.REACT_APP_BACKEND_URL };
+      }
+    else
+      {
+        return { REACT_APP_BACKEND_URL: "http://localhost:8080" };
+      }
   } catch (error) {
     console.error("Error loading runtime config:", error);
-    return {
-      REACT_APP_BACKEND_URL: "https://python-rag-app-369543119888.us-central1.run.app", // Fallback to production
-      environment: "Prod",
-    };
+    return { REACT_APP_BACKEND_URL: "https://python-rag-app-369543119888.us-central1.run.app" }; // Fallback
   }
 };
   
